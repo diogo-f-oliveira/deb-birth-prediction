2c6eae613890ba9cfba012a826a4a012bbf42ada
Diogo Oliveira <diogo.oliveira898@gmail.com>
2026-01-22 15:24:09 +0000

    Custom functions for genetic programming symbolic classifier

--- src/debbirth/models/gp/constants.py
+++ src/debbirth/models/gp/constants.py
@@ -14,8 +14,9 @@ class GPConstant:
 
 
 DEFAULT_CONSTANTS: Tuple[GPConstant, ...] = (
+    GPConstant(name="c1", value=1.0),
     GPConstant(name="c2", value=2.0),
     GPConstant(name="c3", value=3.0),
-    GPConstant(name="c1_2", value=0.5),
-    GPConstant(name="c1_3", value=1.0 / 3.0),
+    # GPConstant(name="c1_2", value=0.5),
+    # GPConstant(name="c1_3", value=1.0 / 3.0),
 )
---
+++ src/debbirth/models/gp/functions.py
@@ -0,0 +1,41 @@
+import numpy as np
+from gplearn.functions import make_function
+
+
+def _cbrt(x: np.ndarray) -> np.ndarray:
+    """Elementwise cubic root (real-valued, supports negatives)."""
+    x = np.asarray(x)
+    return np.cbrt(x)
+
+
+CBRT = make_function(function=_cbrt, name="cbrt", arity=1, wrap=True)
+
+
+def _square(x: np.ndarray) -> np.ndarray:
+    """Elementwise square: x^2."""
+    x = np.asarray(x)
+    return np.square(x)
+
+
+SQUARE = make_function(function=_square, name="square", arity=1, wrap=True)
+
+
+def _cube(x: np.ndarray) -> np.ndarray:
+    """Elementwise cube: x^3."""
+    x = np.asarray(x)
+    return np.power(x, 3)
+
+
+CUBE = make_function(function=_cube, name="cube", arity=1, wrap=True)
+
+
+def _atan(x: np.ndarray) -> np.ndarray:
+    """Elementwise arctangent."""
+    x = np.asarray(x)
+    return np.arctan(x)
+
+
+ATAN = make_function(function=_atan, name="atan", arity=1, wrap=True)
+
+
+DEFAULT_FUNCTIONS = ("add", "sub", "mul", "div", "sqrt", "log", "inv", "neg")
