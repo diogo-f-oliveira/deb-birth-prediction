d6e1683c12d9dd330f116f636bbc68bc03d775f7
Diogo Oliveira <diogo.oliveira898@gmail.com>
2026-01-26 10:26:41 +0000

    Constant set options and typing

--- src/debbirth/models/gp/constants.py
+++ src/debbirth/models/gp/constants.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Tuple
 
+
 @dataclass(frozen=True)
 class GPConstant:
     """A named constant terminal.
@@ -13,10 +14,19 @@ class GPConstant:
     value: float
 
 
-DEFAULT_CONSTANT_SET: Tuple[GPConstant, ...] = (
+GPConstantSet = Tuple[GPConstant, ...]
+
+NO_CONSTANT_SET: GPConstantSet = ()
+
+DEFAULT_CONSTANT_SET: GPConstantSet = (
     GPConstant(name="c1", value=1.0),
     GPConstant(name="c2", value=2.0),
     GPConstant(name="c3", value=3.0),
-    # GPConstant(name="c1_2", value=0.5),
-    # GPConstant(name="c1_3", value=1.0 / 3.0),
+)
+
+EXTENDED_CONSTANT_SET: GPConstantSet = DEFAULT_CONSTANT_SET + (
+    GPConstant(name="c1_2", value=0.5),
+    GPConstant(name="c1_3", value=1.0 / 3.0),
+    GPConstant(name="sqrt2", value=2 ** 0.5),
+    GPConstant(name="sqrt3", value=3 ** 0.5),
 )
